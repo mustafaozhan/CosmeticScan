ea015946857a5cc8d9119a59c79e5afe0e8d1d4f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-07-09 02:17:06 +0300

    Scanning Text part is completed

--- app/src/main/java/mustafaozhan/github/com/cosmeticscan/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/cosmeticscan/MainActivity.kt
@@ -1,12 +1,117 @@
 package mustafaozhan.github.com.cosmeticscan
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var cameraView: SurfaceView
+    internal lateinit var textView: TextView
+    internal lateinit var cameraSource: CameraSource
+    internal val RequestCameraPermissionID = 1001
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            RequestCameraPermissionID -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return
+                    }
+                    try {
+                        cameraSource.start(cameraView.holder)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraView = findViewById(R.id.surface_view) as SurfaceView
+        textView = findViewById(R.id.text_view) as TextView
+
+        val textRecognizer = TextRecognizer.Builder(applicationContext).build()
+        if (!textRecognizer.isOperational()) {
+            Log.w("MainActivity", "Detector dependencies are not yet available")
+        } else {
+
+            cameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build()
+            cameraView.holder.addCallback(object : SurfaceHolder.Callback {
+                override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
+
+                    try {
+                        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(this@MainActivity,
+                                    arrayOf<String>(Manifest.permission.CAMERA),
+                                    RequestCameraPermissionID)
+                            return
+                        }
+                        cameraSource.start(cameraView.holder)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                }
+
+                override fun surfaceChanged(surfaceHolder: SurfaceHolder, i: Int, i1: Int, i2: Int) {
+
+                }
+
+                override fun surfaceDestroyed(surfaceHolder: SurfaceHolder) {
+                    cameraSource.stop()
+                }
+            })
+
+            textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
+                override fun release() {
+
+                }
+
+                override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+
+                    val items = detections.getDetectedItems()
+                    if (items.size() != 0) {
+                        textView.post {
+                            val stringBuilder = StringBuilder()
+                            for (i in 0..items.size() - 1) {
+                                val item = items.valueAt(i)
+                                stringBuilder.append(item.getValue())
+                                stringBuilder.append("\n")
+                            }
+                            textView.text = stringBuilder.toString()
+                        }
+                    }
+                }
+            })
+        }
     }
 }
